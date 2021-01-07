4fdb9aa49affdd1705406cf6838e30a9e9e9258d
Daniel Patyk <daniel.patyk@gmx.de>
2021-01-07 17:21:07 +0100

    Added Openings and BoxTrigger to room and worked on the room spawning
    The adjacent room spawning is not so easy, because I need to find a fitting room by choosing one by random, get a random entrance from that room, rotate the room and check if it fits the position, if so I need to spawn it and check, which openings are connected now or not and mark the right ones as connected. Then I add the new room to the room list of the dungeon generator.

--- 3D Dungeon Generator/Assets/Scripts/Room.cs
+++ 3D Dungeon Generator/Assets/Scripts/Room.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    private List<RoomOpening> m_UnconnectedRoomOpenings = new List<RoomOpening>();
+    [SerializeField] private BoxCollider m_BoxCollider = null;
+    [SerializeField] private List<RoomOpening> m_UnconnectedRoomOpenings = new List<RoomOpening>();
     private List<RoomOpening> m_ConnectedRoomOpenings = new List<RoomOpening>();
     
     private void Awake()
     {
-        
+        foreach (RoomOpening roomOpening in m_UnconnectedRoomOpenings)
+        {
+            roomOpening.SetRoom(this);
+        }
     }
 
+    public BoxCollider GetBoxCollider()
+    {
+        return m_BoxCollider;
+    }
+    
+    public List<RoomOpening> GetUnconnectedRoomOpenings()
+    {
+        return m_UnconnectedRoomOpenings;
+    }
+
+    public void MoveRoomOpeningToConnected(in RoomOpening roomOpening)
+    {
+        m_UnconnectedRoomOpenings.Remove(roomOpening);
+        m_ConnectedRoomOpenings.Add(roomOpening);
+    }
+
+    
+    
     void SpawnAdjacentRooms()
     {
 
--- 3D Dungeon Generator/Assets/Scripts/RoomOpening.cs
+++ 3D Dungeon Generator/Assets/Scripts/RoomOpening.cs
@@ -6,32 +6,76 @@ public class RoomOpening : MonoBehaviour
 {
     [SerializeField] private bool m_Connected = false;
 
-
-    private DungeonGenerator m_DungeonGenerator = null; 
+    private Room m_Room = null;
+    private DungeonGenerator m_DungeonGenerator = null;
+    private List<GameObject> m_RoomPrefabs = null;
     
     private void Awake()
     {
         m_DungeonGenerator = GameObject.Find("DungeonGenerator").GetComponent<DungeonGenerator>();
         if (m_DungeonGenerator != null)
         {
-
+            m_RoomPrefabs = m_DungeonGenerator.GetRoomPrefabList();
         }
+    }
+
+    public void SetRoom(in Room room)
+    {
+        m_Room = room;
+    }
+    
+    public void SpawnAdjacentRoom()
+    {
+        bool fittingRoomFound = false;
+
+        while (!fittingRoomFound)
+        {
+            //Step 1: Get Random room
+            GameObject randomRoomPrefab = m_RoomPrefabs[Random.Range(0, m_RoomPrefabs.Count)];
+            Room randomRoom = randomRoomPrefab.GetComponent<Room>();
+            //Step 2: Get Random room opening
+            List<RoomOpening> roomOpenings = randomRoom.GetUnconnectedRoomOpenings();
+            RoomOpening roomOpening = roomOpenings[Random.Range(0, roomOpenings.Count)];
+            
+            //Step 3: Spawn that room on that random room connected with the choosen Room opening in a way that the room opening is where this room opening is
+            Vector3 localPos = roomOpening.gameObject.transform.localPosition;
+            //Todo: Step 4: Rotate the spawned room so it looks in the right direction (take the rotation into account before spawning)
+
+            Vector3 overlapBoxPosition = gameObject.transform.position - localPos +
+                                         randomRoom.GetBoxCollider().gameObject.transform.localPosition;
+            Collider[] colliders = Physics.OverlapBox(overlapBoxPosition, randomRoom.GetBoxCollider().bounds.extents, Quaternion.identity,
+                LayerMask.NameToLayer("RoomBoundingBox"));
+
+            if (colliders.Length == 0)
+            {
+
+                //Todo: Take roation into account when spawning
+                GameObject spawnedRoomPrefab = Instantiate(randomRoomPrefab, gameObject.transform.position - localPos, Quaternion.identity,
+                    gameObject.transform.parent.parent);
+
+                
+                //Step 6: Check which room openings from the spawned room are now connected with other room openings and mark them as connected
+
+                fittingRoomFound = true;
+            }
+            else
+            {
+                Debug.Log("Could not spawn room");
+            }
+            
+        }
+        
+        
+        
+
+        
+        //(Step 7: Notify original room, that you are done)
+        
+        // spawnedRoomPrefab.GetComponent<Room>();
     }
 
     public bool IsConnected()
     {
         return m_Connected;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
